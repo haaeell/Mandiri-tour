381e8c5e58e6189ec7d64b64ebb6fdbc3a1d2057
haaeell <haikaljawahir434@gmail.com>
2024-05-24 00:00:43 +0700

    fix: verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -9,19 +9,19 @@
                     <h4 class="mb-0 text-center fw-semibold">{{ __('Verifikasi Alamat Email Anda') }}</h4>
                 </div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('Tautan verifikasi baru telah dikirim ke alamat email Anda.') }}
                         </div>
                     @endif
 
-                    <p>{{ __('Sebelum melanjutkan, harap periksa email Anda untuk tautan verifikasi.') }}</p>
-                    <p class="mb-0 mt-4">{{ __('Jika Anda tidak menerima email tersebut') }},</p>
+                    <p class="mt-4 fw-semibold">{{ __('Sebelum melanjutkan, harap periksa email Anda untuk tautan verifikasi.') }}</p>
+                    <p class="mb-0 mt-5 fw-semibold">{{ __('Jika Anda tidak menerima email tersebut') }},</p>
                     
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-primary p-2 mt-2 align-baseline">{{ __('klik di sini untuk meminta lagi') }}</button>.
+                        <button type="submit" class="btn bg-primary p-3 text-white btn-sm fw-semibold mt-2 align-baseline">{{ __('klik di sini untuk meminta lagi') }}</button>.
                     </form>
                 </div>
             </div>
